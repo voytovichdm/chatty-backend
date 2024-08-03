a441c51ffba8892e82cd94164d65fb19c196e35d
VoitovychDM <voitovych.dm.m@gmail.com>
2024-08-03 09:21:35 +0200

    feat: implemented password reset feature with unit test

---
+++ src/features/auth/controllers/password.ts
@@ -0,0 +1,64 @@
+import { Request, Response } from 'express';
+import moment from 'moment';
+import publicIP from 'ip';
+import HTTP_STATUS from 'http-status-codes';
+import crypto from 'crypto';
+import { joiValidation } from '../../../shared/globals/decorators/joi-validation.decorators';
+import { emailSchema, passwordSchema } from '../schemes/password';
+import { IAuthDocument } from '../interfaces/auth.interface';
+import { authService } from '../../../shared/globals/services/db/auth.service';
+import { BadRequestError } from '../../../shared/globals/helpers/error-handler';
+import { config } from '../../../config';
+import { forgotPasswordTemplate } from '../../../shared/globals/services/emails/templates/forgot-password/forgot-password-template';
+import { emailQueue } from '../../../shared/globals/services/queues/email.queue';
+import { IResetPasswordParams } from '../user/interfaces/user.interface';
+import { resetPasswordTemplate } from '../../../shared/globals/services/emails/templates/reset-password/reset-password-template';
+
+
+export class Password {
+  @joiValidation(emailSchema)
+  public async create(req: Request, res: Response): Promise<void> {
+    const { email } = req.body;
+    const existingUser: IAuthDocument = await authService.getAuthUserByEmail(email);
+    if (!existingUser) {
+      throw new BadRequestError('Invalid credentials');
+    }
+
+    const randomBytes: Buffer = await Promise.resolve(crypto.randomBytes(20));
+    const randomCharacters: string = randomBytes.toString('hex');
+    await authService.updatePasswordToken(`${existingUser._id!}`, randomCharacters, Date.now() * 60 * 60 * 1000);
+
+    const resetLink = `${config.CLIENT_URL}/reset-password?token=${randomCharacters}`;
+    const template: string = forgotPasswordTemplate.passwordResetTemplate(existingUser.username!, resetLink);
+    emailQueue.addEmailJob('forgotPasswordEmail', { template, receiverEmail: email, subject: 'Reset your password' });
+    res.status(HTTP_STATUS.OK).json({ message: 'Password reset email sent.' });
+  }
+
+  @joiValidation(passwordSchema)
+  public async update(req: Request, res: Response): Promise<void> {
+    const { password, confirmPassword } = req.body;
+    const { token } = req.params;
+    if (password !== confirmPassword) {
+      throw new BadRequestError('Passwords do not match');
+    }
+    const existingUser: IAuthDocument = await authService.getAuthUserByPasswordToken(token);
+    if (!existingUser) {
+      throw new BadRequestError('Reset token has expired.');
+    }
+
+    existingUser.password = password;
+    existingUser.passwordResetExpires = undefined;
+    existingUser.passwordResetToken = undefined;
+    await existingUser.save();
+
+    const templateParams: IResetPasswordParams = {
+      username: existingUser.username!,
+      email: existingUser.email!,
+      ipaddress: publicIP.address(),
+      date: moment().format('DD//MM//YYYY HH:mm')
+    };
+    const template: string = resetPasswordTemplate.passwordResetConfirmationTemplate(templateParams);
+    emailQueue.addEmailJob('forgotPasswordEmail', { template, receiverEmail: existingUser.email!, subject: 'Password Reset Confirmation' });
+    res.status(HTTP_STATUS.OK).json({ message: 'Password successfully updated.' });
+  }
+}
--- src/features/auth/routes/authRoutes.ts
+++ src/features/auth/routes/authRoutes.ts
@@ -1,3 +1,4 @@
+import { Password } from '../controllers/password';
 import { SignIn } from '../controllers/signin';
 import { SignOut } from '../controllers/signout';
 import { SignUp } from '../controllers/signup';
@@ -13,6 +14,8 @@ class AuthRoutes {
   public routes(): Router {
     this.router.post('/signup', SignUp.prototype.create);
     this.router.post('/signin', SignIn.prototype.read);
+    this.router.post('/forgot-password', Password.prototype.create);
+    this.router.post('/reset-password/:token', Password.prototype.update);
 
     return this.router;
   }
